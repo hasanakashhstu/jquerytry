41b1d95a2dea90eb3aa61b73837d4f8a306ef191
kamrulhasan0215 <kamrulhasan0215@gmail.com>
2021-06-06 18:02:13 +0600

    1.2

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
 $(document).ready(function(){
-//alert('ready');
+//$('.class1 , .class2').addClass('bold');
+//$('#list li').addClass('bold');
+//$('#list > li').addClass('bold');
+//$('input[required]').addClass('bold2');
+$('input[id*=name]').addClass('bold2');
 });
